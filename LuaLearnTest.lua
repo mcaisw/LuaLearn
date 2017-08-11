04643b0a1a583bbda91b6690580c756baadacae3
MengChi <mc2311362372@gmail.com>
2017-08-11 19:25:40 +0800

    ..

--- LuaLearnTest.lua
+++ LuaLearnTest.lua
@@ -1,16 +1,22 @@
 print('------------------面向对象-----------------------------')
 MyClass={}
 MyClass.__index=MyClass
-MyClass.new=function(name,num)
+MyClass.new=function(name,num)--函数定义，有两种方式
 	local this=setmetatable({},MyClass)
 	this.name=name
 	this.num=num
 	return this
 end
 
+--第二种方式的函数定义
+MyClass.PrintInfo=function(this)
+	print(this.name,this.num)
+end
+
 mc=MyClass.new('mengchi',25)--实例化对象
 print(type(mc))
 print(mc.num,mc.name)
+mc.PrintInfo(mc)
 
 print('------------------—index元方法-----------------------------')
 MyClassTest={newValue=0}--newValue可以在MyClass找到
